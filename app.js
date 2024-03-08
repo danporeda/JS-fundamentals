a6f2e99c5f3929a92f230721cb0913bf9fdb851d
Dan <danporeda@yahoo.com>
2024-03-08 12:15:01 -0500

    while loops

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
-const num = Math.floor(Math.random() * 5);
-const days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+// const num = Math.floor(Math.random() * 5);
+// const days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
 
-switch (num) {
-  case 1: 
-    console.log("1");
-    break;
-  case 2: 
-    console.log("2");
-    break;
-  case 3: 
-    console.log("3");
-    break;
-  case 4: 
-    console.log("4");
-    break;
-  default:
-    console.log("0")
-};
+// switch (num) {
+//   case 1: 
+//     console.log("1");
+//     break;
+//   case 2: 
+//     console.log("2");
+//     break;
+//   case 3: 
+//     console.log("3");
+//     break;
+//   case 4: 
+//     console.log("4");
+//     break;
+//   default:
+//     console.log("0")
+// };
 
-switch (days[num]) {
-  case "monday":
-    console.log("Monday");
-    break;
-  case "tuesday":
-    console.log("Tuesday");
-    break;
-  case "wednesday":
-    console.log("Wednesday");
-    break;
-  case "thursday":
-    console.log("Thursday");
-    break;
-  default:
-    console.log("Friday");
+// switch (days[num]) {
+//   case "monday":
+//     console.log("Monday");
+//     break;
+//   case "tuesday":
+//     console.log("Tuesday");
+//     break;
+//   case "wednesday":
+//     console.log("Wednesday");
+//     break;
+//   case "thursday":
+//     console.log("Thursday");
+//     break;
+//   default:
+//     console.log("Friday");
+// }
+
+// const numbers = [2,45,83,904,6,2,900];
+// console.log(numbers);
+// const sortedNum = numbers.sort((a,b) => {
+//   return a - b;
+// });
+// console.log(sortedNum);
+
+
+const secret = 'CUNT';
+
+let guess = prompt("enter secret code");
+while (guess !== secret) {
+  guess = prompt("enter secret code");
 }
