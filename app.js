2b8a0ce87f372c21d66def0d5ff9687c620b89c7
Dan <danporeda@yahoo.com>
2024-03-08 16:16:48 -0500

    guessing game

--- app.js
+++ app.js
@@ -43,9 +43,63 @@
 // console.log(sortedNum);
 
 
-const secret = 'CUNT';
+// const secret = 'punt';
 
-let guess = prompt("enter secret code");
-while (guess !== secret) {
-  guess = prompt("enter secret code");
+// let guess = prompt("enter secret code");
+// while (guess !== secret) {
+//   guess = prompt("enter secret code");
+// }
+
+// console.log("Congrats, you got it");
+
+// let input = prompt('say something');
+
+
+// WHILE LOOPS
+
+// while(true) {
+//   input = prompt(input);
+//   if (input === "stop copying me") {
+//     break;
+//   }
+// }
+
+let maximum = parseInt(prompt("Enter maximum number"));
+while (!maximum) {
+  maximum = parseInt(prompt("enter a number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = prompt("Enter your first guess (enter 'q' to quit)");
+let guesses;
+if (guess === 'q' || !(parseInt(guess))) {
+  guesses = 0;
+} else { guesses = 1; }
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === 'q') break;
+  guess = parseInt(guess);
+  if (guess > targetNum) {
+    guess = prompt("too High, guess again");
+    if (guess === 'q') break;
+    if (parseInt(guess)) guesses++;
+  } else if (guess < targetNum) {
+    guess = prompt("too low, guess again");
+    if (guess === 'q') break;
+    if (parseInt(guess)) guesses++;
+  } else {
+    while (!guess) {
+      guess = prompt("Invalid input, Please enter a number or 'q' to quit.")
+      if (guess === 'q') break;
+      guess = parseInt(guess);
+    }
+    if (guess !== 'q') { guesses++ };
+  }
+}
+
+if (guess === 'q') {
+  console.log(`You cunt quitter. ${guesses} guesses.`)
+} else {
+console.log(`you got the cunt! The number is ${targetNum}, took you ${guesses} guesses, you cunt`);
 }
