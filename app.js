84ae1399ec1111d72e8ab4d5b5f540fd0aff31d7
Dan <danporeda@yahoo.com>
2024-03-13 19:04:47 -0600

    reduce method

--- app.js
+++ app.js
@@ -106,3 +106,99 @@
 // } else {
 // console.log(`you got the cunt! The number is ${targetNum}, took you ${guesses} guesses, you cunt`);
 // }
+
+
+// FUNCTIONS---------
+
+// function yell(msg) {
+//   try {
+//     console.log(msg.toUpperCase().repeat(3));
+//   } catch (err) {
+//     console.log(`${err} ya fucktunt`);
+//   }
+// }
+
+// yell(3);
+
+
+
+// FOREACH---------
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const movies = [
+  {
+    title: "Babe",
+    score: 100,
+    year: 1994
+  },
+  {
+    title: "Alien",
+    score: 90,
+    year: 2006
+  },
+  {
+    title: "Pulp Fiction",
+    score: 98,
+    year: 1995
+  }
+];
+
+// numbers.forEach((el) => {
+//   if (el % 2 === 0)
+//   console.log(`${el} is even`);
+// })
+
+// movies.forEach((mov) => {
+//   console.log(`${mov.title} - ${mov.score}`);
+// })
+
+// const powers = numbers.map((num) => {
+//   console.log(num ** num);
+//   return num ** num;
+// })
+
+// const movieTitles = movies.map((mov) => {
+//   return mov.title;
+// })
+
+// console.log("Hello")
+// setTimeout(() => {
+//   console.log('Still there?')
+// }, 2000);
+// console.log('goodbye');
+
+// const id = setInterval(() => {
+//   console.log(Math.random())
+//   console.log(id);
+// }, 500);
+
+// setTimeout(() => {
+//   clearInterval(id);
+// }, 5000)
+
+
+// const evens = numbers.filter(num => num % 2 === 0);
+// const odds = numbers.filter(num => num % 2 !== 0);
+
+// console.log(`Evens ${evens}`);
+// console.log(`Odds ${odds}`);
+
+// const goodTitles = movies.filter(mov => mov.score > 90).map(mov => mov.title);
+// console.log(`good titles: ${goodTitles}`);
+
+const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+
+const total = prices.reduce((acc, curr) => acc + curr, 0);
+console.log(`total: ${total}`);
+
+const manTotal = 9.99 + 1.50 + 19.99 + 49.99 + 30.50;
+console.log(manTotal);
+
+const getAverage = (numberArray) => {
+  const total = prices.reduce((acc, curr) => acc + curr, 0);
+  return total / numberArray.length;
+}
+
+console.log("average:");
+console.log(getAverage(prices));
