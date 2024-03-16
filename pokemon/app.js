a391dde6e9799d52ae0a43137057d33135da0cea
Dan <danporeda@yahoo.com>
2024-03-16 11:28:21 -0600

    completed pokemon exercise

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,8 +1,16 @@
 const target = document.querySelector('.container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for (let i = 1; i < 151; i++) {
-  let img = document.createElement('img');
-  img.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`);
-  target.appendChild(img);
+for (let i = 1; i < 201; i++) {
+  const pokemon = document.createElement('div');
+  pokemon.classList.add('pokemon');
+  const label = document.createElement('span');
+  const img = document.createElement('img');
+  img.setAttribute('src', `${baseURL}${i}.png`);
+  // img.src = `${baseURL}${i}.png`;
+  label.innerText = `(${i})`;
+  pokemon.appendChild(img);
+  pokemon.appendChild(label);
+  target.appendChild(pokemon);
 }
 
