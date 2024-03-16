e0e6d082de9d218d22121f8607ceb8c3404bd131
Dan <danporeda@yahoo.com>
2024-03-15 22:29:48 -0600

    insertAdjacentElements

--- dom/app.js
+++ dom/app.js
@@ -6,7 +6,19 @@ const allLinks = document.querySelectorAll('a');
 
 
 for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
+    link.style.color = 'rgb(0,108,134)';
     link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
+    link.style.textDecorationStyle = 'wavy';
 }
+
+const h1 = document.querySelector('h1');
+const positions = ['beforebegin', 'afterbegin', 'beforeend', 'afterend'];
+const elements = [];
+for (let i of positions ) {
+  let el = document.createElement('h2');
+  el.append(i);
+  // el.innerText = i;
+  h1.insertAdjacentElement(i, el);
+}
+
+// const img = document.querySelector('#banner');
