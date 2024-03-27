fe53d5ecebb21cb1e72f90e4464c1fe05d2be7a3
Dan <danporeda@yahoo.com>
2024-03-26 21:38:24 -0400

    fetch method

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,7 +1,7 @@
 const target = document.querySelector('.container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-for (let i = 1; i < 201; i++) {
+for (let i = 1; i < 1026; i++) {
   const pokemon = document.createElement('div');
   pokemon.classList.add('pokemon');
   const label = document.createElement('span');
